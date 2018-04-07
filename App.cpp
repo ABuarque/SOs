5bce50998ff926e619479325ce473c2d0039cd0f
ABuarque <abuarquemf@gmail.com>
2018-04-07 12:10:51 -0300

    added function to show specif record

--- App.cpp
+++ App.cpp
@@ -42,6 +42,8 @@ bool shouldShowFeedBackMessage = false;
 //////////////////////////APP's ToDos///////////////////
 void insertRecord();
 void deleteRecord();
+void modifyFieldInRecord();
+void displayRecord();
 /////////////////////////////////////////////////////
 
 void sequenceSetEntryPoint() {
@@ -81,10 +83,12 @@ void sequenceSetEntryPoint() {
 				deleteRecord();
 				break;
 			case MODIFY_FIELD_IN_RECORD:
+				modifyFieldInRecord();
 				break;
 			case DISPLAY_SPECIFIC_FIELD:
 				break;
 			case DISPLAY_RECORD:
+				displayRecord();
 				break;
 			case SHOW_STATE:
 				break;
@@ -139,6 +143,81 @@ void deleteRecord() {
 		feedBackMessage = "There are no records inside the set.";
 	}
 }
+
+void modifyFieldInRecord() {
+	if(sequenceSet->getTotalRecordsInsideSequenceSet() > 0) {
+		cout << "Type record zip code: ";
+		long zipCode;
+		cin >> zipCode;
+		getchar();
+		Record* recordToChange = sequenceSet->queryRecordByZipCode(zipCode);
+		while(recordToChange == NULL) {
+			cout << "Was not found any record with given zip code. Type it again: \n";
+			cout << ">>> ";
+			cin >> zipCode;
+			getchar();
+			recordToChange = sequenceSet->queryRecordByZipCode(zipCode);
+		}
+		cout << "Type what you want to change: \n";
+		cout << "\t1 Place name" << endl;
+		cout << "\t2 State" << endl;
+		cout << "\t3 County" << endl;
+		cout << "\t4 Latitute" << endl;
+		cout << "\t5 Longitude" << endl;
+		cout << ">>> ";
+		bool isTryingToGetInput = true;
+		string input;
+		int option;
+		while(isTryingToGetInput) {
+			try {
+				getline(cin, input);
+				option = stoi(input);
+				getchar();
+				isTryingToGetInput = false;
+			} catch(...) {
+				cout << "Invalid input\n";
+			}
+	    }
+
+	    sequenceSet->removeRecordByZipCode(zipCode);
+
+	    switch(option) {
+	    	case 1:
+	    		cout << "Type new place name: ";
+	    		string placeName;
+	    		getline(cin, placeName);
+	    		recordToChange->setPlaceName(placeName);
+	    		sequenceSet->addRecord(recordToChange);
+	    		break;
+	    }
+	} else {
+		shouldShowFeedBackMessage = true;
+		feedBackMessage = "There are no records inside the set.";
+	}
+}
+
+void displayRecord() {
+	if(sequenceSet->getTotalRecordsInsideSequenceSet() > 0) {
+		cout << "Type record zip code: ";
+		long zipCode;
+		cin >> zipCode;
+		getchar();
+		Record* record = sequenceSet->queryRecordByZipCode(zipCode);
+		cout << "Record resume: \n";
+		cout << "\tZip code: " << record->getZipCode() << endl;
+		cout << "\tPlace name: " << record->getPlaceName() << endl;
+		cout << "\tState: " << record->getState() << endl;
+		cout << "\tCounty: " << record->getCounty() << endl;
+		cout << "\tLatitude: " << record->getLatitute() << endl;
+		cout << "\tLongitude: " << record->getLongitude() << endl;
+		cout << "Press any key to get back";
+		string input;
+		getline(cin, input);
+	} else {
+		shouldShowFeedBackMessage = true;
+		feedBackMessage = "There are no records inside the set.";
+	}
+}	
 
 void showHeader() {
 	string* fieldsNames = sequenceSet->getFieldsNames();
