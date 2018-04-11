165a9433c2fb7a0dc537057ee054c6b4ee56ca95
ABuarque <abuarquemf@gmail.com>
2018-04-11 17:37:23 -0300

    fixed bug

--- utils.cpp
+++ utils.cpp
@@ -1,11 +1,17 @@
+#include "utils.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <string>
 
-long stringToLong(char* cLikeString) {
-	return strtol(cLikeString, NULL, 10);
+long stringToLong(std::string cLikeString) {
+	char* parsedString = new char[cLikeString.size() + 1];
+	strcpy(parsedString, cLikeString.c_str());
+	return strtol(parsedString, NULL, 10);
 }
 
-float stringToFloat(char* cLikeString) {
-	return strtod(cLikeString, NULL);
+float stringToFloat(std::string cLikeString) {
+	char* parsedString = new char(cLikeString.size() + 1);
+	strcpy(parsedString, cLikeString.c_str());
+	return strtod(parsedString, NULL);
 }
--- utils.h
+++ utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <string>
+
 /**
  * It parses a c like string (using char*) to 
  * a long in base 10.
@@ -8,7 +10,7 @@
  * @param c like string
  * @return long in base 10
  */
-long stringToLong(char* cLikeString);
+long stringToLong(std::string cLikeString);
 
 /**
  * It parses a c like string (using char*) 
@@ -17,6 +19,6 @@ long stringToLong(char* cLikeString);
  * @param c like string
  * @retunr string as a float
  */
-float stringToFloat(char* cLikeString);
+float stringToFloat(std::string cLikeString);
 
 #endif
